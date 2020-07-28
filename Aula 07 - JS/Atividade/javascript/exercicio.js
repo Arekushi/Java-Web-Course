22cec4e902cd9bb4d7bf7a47d12f768a2b0a9ca9
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2020-07-27 22:30:01 -0300

    :rocket: Add lessons

---
+++ Aula 07 - JS/Atividade/javascript/exercicio.js
@@ -0,0 +1,28 @@
+var integerPositive = 10;
+var integerNegative = -20;
+var doubleNumber = 20.5;
+var stringName = "Arekushi"
+var booleanValue = false;
+
+console.log(integerPositive);
+console.log(integerNegative);
+console.log(doubleNumber);
+console.log(stringName);
+console.log(booleanValue);
+
+var name = "Alexandre";
+var lastName = "Lima";
+console.log(name.concat(" ", lastName));
+console.log(name, lastName);
+
+var nullableVar = null;
+var undefinedVar = undefined;
+
+console.log(nullableVar);
+console.log(undefinedVar);
+
+var fruit = ["Abacaxi", "Açai", "Amora", ('b' + 'a'+ + 'a' + 'a').toLowerCase(), "Cereja"]
+console.log(fruit);
+
+var text = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonham"
+console.log(text);
